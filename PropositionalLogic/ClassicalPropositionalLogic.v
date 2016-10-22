246b10e71b4e43d3cb51142c59f53d0e0b6e5f20
Qinxiang <caoqinxiang@gmail.com>
2016-10-22 12:50:03 -0400

    More definitions for general propositional logic.

--- PropositionalLogic/ClassicalPropositionalLogic.v
+++ PropositionalLogic/ClassicalPropositionalLogic.v
@@ -32,17 +32,40 @@ Instance ClassicalPropositionalLogic_ProofTheory (Gamma: ClassicalPropositionalL
 
 End ClassicalPropositionalLogicSection.
 
-Module ClassicalPropositionalLogic.
+Implicit Arguments ClassicalPropositionalLogic.
 
-Inductive provable: expr Var -> Prop :=
-| syntactic_reduction_rule: forall x y, reduce x y -> provable x -> provable y
+Local Open Scope logic_base.
+
+Existing Instance ClassicalPropositionalLogic_ProofTheory.
+
+Lemma imp_refl: forall {L: PropositionalLanguage} {Gamma: ClassicalPropositionalLogic L} (x: expr), |-- (x --> x).
+Proof.
+  intros.
+  pose proof axiom2 x (x --> x) x.
+  pose proof axiom1 x (x --> x).
+  pose proof axiom1 x x.
+  pose proof modus_ponens _ _ H H0.
+  pose proof modus_ponens _ _ H2 H1.
+  auto.
+Qed.
+
+Module ClassicalPropositionalLogic.
+Section ClassicalPropositionalLogic.
+Context (Var: Type).
+
+Definition CPL := PropositionalLanguage.PropositionalLanguage Var.
+Existing Instance CPL.
+
+Inductive provable: PropositionalLanguage.expr Var -> Prop :=
+| syntactic_reduction_rule: forall x y: PropositionalLanguage.expr Var, reduce x y -> provable x -> provable y
 | modus_ponens: forall x y, provable (x --> y) -> provable x -> provable y
 | axiom1: forall x y, provable (x --> (y --> x))
 | axiom2: forall x y z, provable ((x --> y --> z) --> (x --> y) --> (x --> z))
 | axiom3: forall x y, provable ((~~ y --> x) --> (~~ y --> ~~ x) --> y).
 
+Instance ClassicalPropositionLogic: ClassicalPropositionalLogic _ :=
+  Build_ClassicalPropositionalLogic provable syntactic_reduction_rule modus_ponens axiom1 axiom2 axiom3.
+
+End ClassicalPropositionalLogic.
 End ClassicalPropositionalLogic.
 
-Instance ClassicalPropositionLogic (Var: Type): ProofTheory (PropositionalLanguage Var) :=
-  AxiomaticProofTheory_ProofTheory
-   (AxiomaticProofTheory.Build_ProofTheory (PropositionalLanguage Var) provable).
