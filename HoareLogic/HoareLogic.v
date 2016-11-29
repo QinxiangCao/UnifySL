6086fe0977946d65d5141f4fc3d9652c42bd09d7
QinxiangCao <caoqinxiang@gmail.com>
2016-11-29 10:52:54 -0500

    Add soundness of frame rule.

--- HoareLogic/HoareLogic.v
+++ HoareLogic/HoareLogic.v
@@ -1,88 +1,90 @@
 Require Import Logic.MinimunLogic.LogicBase.
+Require Import Logic.PropositionalLogic.Syntax.
+Require Import Logic.SeparationLogic.Syntax.
 Require Import Logic.HoareLogic.ImperativeLanguage.
+Require Import Logic.PropositionalLogic.KripkeSemantics.
+Require Import Logic.SeparationLogic.SeparationAlgebra.
+Require Import Logic.SeparationLogic.Semantics.
 
 Class HoareLogic (Imp: ImperativeProgrammingLanguage) (L: Language): Type := {
   triple: expr -> cmd -> expr -> Prop
 }.
 
 Local Open Scope logic_base.
-(*
-Definition triple_valid {Imp: ImperativeProgrammingLanguage} {nImp: NormalImperativeProgrammingLanguage Imp} {L: Language} {MD: Model} {sss: SmallStepSemantics Imp MD} {SM: Semantics L MD} (guard: exceptional_state -> Prop) (Pre: expr) (c: cmd) (Post: expr): Prop :=
-  forall (s_pre: state) (ms_post: state + exceptional_state), access (inl s_pre) c ms_post -> s_pre |= Pre ->
+Local Open Scope KripkeSemantics.
+
+Definition triple_partial_valid {Imp: ImperativeProgrammingLanguage} {L: Language} {MD: Model} {kMD: KripkeModel MD} {M: Kmodel} {BSS: BigStepSemantics Imp (Kworlds M)} {SM: Semantics L MD} (Pre: expr) (c: cmd) (Post: expr): Prop :=
+  forall (s_pre: Kworlds M) (ms_post: MetaState (Kworlds M)),
+  (build_model M s_pre) |= Pre ->
+  access s_pre c ms_post ->
   match ms_post with
-  | inl s_post => s_post |= Post
-  | inr e => guard e
+  | Error => False
+  | NonTerminating => True
+  | Terminating s_post => build_model M s_post |= Post
+  end.
+
+Definition triple_total_valid {Imp: ImperativeProgrammingLanguage} {L: Language} {MD: Model} {kMD: KripkeModel MD} {M: Kmodel} {BSS: BigStepSemantics Imp (Kworlds M)} {SM: Semantics L MD} (Pre: expr) (c: cmd) (Post: expr): Prop :=
+  forall (s_pre: Kworlds M) (ms_post: MetaState (Kworlds M)),
+  (build_model M s_pre) |= Pre ->
+  access s_pre c ms_post ->
+  match ms_post with
+  | Error => False
+  | NonTerminating => False
+  | Terminating s_post => build_model M s_post |= Post
   end.
 
 Section soundness.
 
-Context {Imp: ImperativeProgrammingLanguage} {nImp: NormalImperativeProgrammingLanguage Imp} {MD: Model} {sss: SmallStepSemantics Imp MD}.
+Context {Imp: ImperativeProgrammingLanguage} {MD: Model} {kMD: KripkeModel MD} {M: Kmodel} {BSS: BigStepSemantics Imp (Kworlds M)} {nBSS: NormalBigStepSemantics Imp (Kworlds M) BSS} {SA: SeparationAlgebra (Kworlds M)} {SA_BSS: SABigStepSemantics Imp (Kworlds M) BSS}.
 
-Lemma exception_go_nowhere: forall e c ms,
-  access (inr e) c ms <->
-  ms = inr e.
+Context {L: Language} {nL: NormalLanguage L} {pL: PropositionalLanguage L} {SL: SeparationLanguage L} {SM: Semantics L MD} {kiM: KripkeIntuitionisticModel (Kworlds M)} {kiSM: KripkeIntuitionisticSemantics L MD M SM} {fsSM: FlatSemantics.FlatSemantics L MD M SM}.
+
+Local Open Scope logic_base.
+Local Open Scope PropositionalLogic.
+Local Open Scope SeparationLogic.
+
+Lemma hoare_frame_partial_sound: forall c P Q F,
+  triple_partial_valid P c Q ->
+  triple_partial_valid (P * F) c (Q * F).
 Proof.
   intros.
-  split; intros.
-  + hnf in H.
-    remember (fmap_pair_cmd (inr e) c) as mcs1 eqn:?H.
-    remember (fmap_pair_cmd ms Sskip) as mcs2 eqn:?H.
-    induction H.
-    - subst.
-      destruct ms; inversion H1.
-      subst; auto.
-    - subst.
-      inversion H0.
-  + subst.
-    hnf.
-    simpl.
-    apply iter_step_refl.
+  unfold triple_partial_valid in *.
+  intros s ms ? ?.
+  rewrite FlatSemantics.sat_sepcon in H0.
+  destruct H0 as [m [f [? [? ?]]]].
+  assert (safe m c) by exact (H m Error H2).
+  destruct ms as [| | n'].
+  + pose proof safety_preserve _ _ _ c H0.
+    revert H1; apply H5; auto.
+  + auto.
+  + destruct (frame_property _ _ _ _ _ H0 H4 H1) as [n [? ?]].
+    rewrite FlatSemantics.sat_sepcon.
+    exists n, f.
+    split; [| split]; auto.
+    apply (H m _ H2 H6).
 Qed.
 
-Lemma aux_sequence_sound: forall c1 c2 ms1 ms3,
-  access ms1 (Ssequence c1 c2) ms3 ->
-  exists ms2,
-  access ms1 c1 ms2 /\ access ms2 c2 ms3.
+Lemma hoare_frame_total_sound: forall c P Q F,
+  triple_total_valid P c Q ->
+  triple_total_valid (P * F) c (Q * F).
 Proof.
   intros.
-  destruct ms1 as [s1 | e1].
-  Focus 2. {
-    exists (inr e1).
-    rewrite exception_go_nowhere in H.
-    subst.
-    split; rewrite exception_go_nowhere; auto.
-  } Unfocus.
-  unfold access in H.
-  remember (fmap_pair_cmd (inl s1) (Ssequence c1 c2)) as mcs1 eqn:?H.
-  remember (fmap_pair_cmd ms3 Sskip) as mcs2 eqn:?H.
-  revert c1 H0.
-  induction H; intros; subst.
-  + destruct ms3 as [s3 | e3]; inversion H0.
-    apply neq_Sskip_Ssequence in H1; contradiction.
-  + rename IHiter_step into IH.
-    specialize (IH eq_refl).
-    inversion H2; subst; clear H2.
-
-Context {L: Language} {SM: Semantics L MD} (guard: exceptional_state -> Prop).
-
-Lemma hoare_sequence_sound: forall c1 c2 P Q R,
-  triple_valid guard P c1 Q ->
-  triple_valid guard Q c2 R ->
-  triple_valid guard P (Ssequence c1 c2) R.
-Proof.
-  intros.
-  hnf; intros.
-  unfold access in H1.
-  remember (fmap_pair_cmd (inl s_pre) (Ssequence c1 c2)) as mcs1 eqn:?H.
-  remember (fmap_pair_cmd ms_post Sskip) as mcs2 eqn:?H.
-  revert P c1 H H2 H3.
-  induction H1; intros; subst.
-  + destruct ms_post as [s_post | e]; [| inversion H3].
-    inversion H3; clear H3.
-    apply neq_Sskip_Ssequence in H4; contradiction.
-  + rename IHiter_step into IH.
-    specialize (IH eq_refl).
-Abort.
+  unfold triple_partial_valid in *.
+  intros s ms ? ?.
+  rewrite FlatSemantics.sat_sepcon in H0.
+  destruct H0 as [m [f [? [? ?]]]].
+  assert (safe m c) by exact (H m Error H2).
+  assert (term_norm m c) by exact (conj (H m Error H2) (H m NonTerminating H2)).
+  destruct ms as [| | n'].
+  + pose proof safety_preserve _ _ _ c H0.
+    revert H1; apply H6; auto.
+  + pose proof terminate_preserve _ _ _ c H0.
+    revert H1; apply H6; auto.
+  + destruct (frame_property _ _ _ _ _ H0 H4 H1) as [n [? ?]].
+    rewrite FlatSemantics.sat_sepcon.
+    exists n, f.
+    split; [| split]; auto.
+    apply (H m _ H2 H7).
+Qed.
 
 End soundness.
-*)
--- HoareLogic/ImperativeLanguage.v
+++ HoareLogic/ImperativeLanguage.v
@@ -5,58 +5,32 @@ Class ImperativeProgrammingLanguage: Type := {
   cmd: Type
 }.
 
-Class BigStepSemantics (Imp: ImperativeProgrammingLanguage) (MD: Model): Type := {
-  state := model;
-  exceptional_state: Type;
-  access: state + exceptional_state -> cmd -> state + exceptional_state -> Prop
+Inductive MetaState (state: Type): Type :=
+  | Error: MetaState state
+  | NonTerminating: MetaState state
+  | Terminating: state -> MetaState state.
+
+Arguments Error {_}.
+Arguments NonTerminating {_}.
+Arguments Terminating {_} _.
+
+Class BigStepSemantics (Imp: ImperativeProgrammingLanguage) (state: Type): Type := {
+  access: state -> cmd -> MetaState state -> Prop
 }.
 
-(*
+Definition safe {Imp: ImperativeProgrammingLanguage} {state: Type} {BSS: BigStepSemantics Imp state} (s: state) (c: cmd): Prop :=
+  ~ access s c Error.
 
-Class FrameBigStepSemantics (Imp: ImperativeProgrammingLanguage) (MD: Model) {kMD: KripkeModel MD} {SA: forall M: Kmodel, SeparationAlgebra MD M} (BSS: BigStepSemantics Imp MD): Type := {
-  frame_property: forall 
-}.
-*)
-(*
-Class NormalImperativeProgrammingLanguage (Imp: ImperativeProgrammingLanguage): Type := {
-  Ssequence: cmd -> cmd -> cmd;
-  Sskip: cmd;
-  neq_Sskip_Ssequence: forall c1 c2, Sskip <> Ssequence c1 c2
+Definition term_norm {Imp: ImperativeProgrammingLanguage} {state: Type} {BSS: BigStepSemantics Imp state} (s: state) (c: cmd): Prop :=
+  ~ access s c Error /\ ~ access s c NonTerminating.
+
+Class NormalBigStepSemantics (Imp: ImperativeProgrammingLanguage) (state: Type) (BSS: BigStepSemantics Imp state): Type := {
+  access_defined: forall s c, exists ms, access s c ms
 }.
 
-Class SmallStepSemantics (Imp: ImperativeProgrammingLanguage) (MD: Model): Type := {
-  state := model;
-  exceptional_state: Type;
-  step: cmd * state -> cmd * state + exceptional_state -> Prop
+Class SABigStepSemantics (Imp: ImperativeProgrammingLanguage) (state: Type) {SA: SeparationAlgebra state} (BSS: BigStepSemantics Imp state): Type := {
+  safety_preserve: forall m mf m' c, join m mf m' -> safe m c -> safe m' c;
+  terminate_preserve: forall m mf m' c, join m mf m' -> term_norm m c -> term_norm m' c;
+  frame_property: forall m mf m' c n', join m mf m' -> safe m c -> access m' c (Terminating n') -> exists n, join n mf n' /\ access m c (Terminating n)
 }.
 
-Definition fmap_sum_left {A1 A2 B: Type} (f: A1 -> A2) (x: A1 + B): A2 + B :=
-  match x with
-  | inl a => inl (f a)
-  | inr b => inr b
-  end.
-
-Definition fmap_Sseq {Imp: ImperativeProgrammingLanguage} {nImp: NormalImperativeProgrammingLanguage Imp} {MD: Model} {sss: SmallStepSemantics Imp MD} (mcs: cmd * state + exceptional_state) (c0: cmd) :=
-  fmap_sum_left (fun cs: cmd * state => let (c, s) := cs in (Ssequence c c0, s)) mcs.
-
-Definition fmap_pair_cmd {Imp: ImperativeProgrammingLanguage} {nImp: NormalImperativeProgrammingLanguage Imp} {MD: Model} {sss: SmallStepSemantics Imp MD} (ms: state + exceptional_state) (c: cmd) :=
-  fmap_sum_left (pair c) ms.
-
-Class NormalSmallStepSemantics (Imp: ImperativeProgrammingLanguage) {nImp: NormalImperativeProgrammingLanguage Imp} (MD: Model) (sss: SmallStepSemantics Imp MD): Type := {
-  step_Ssequence1: forall c1 c2 s1 mcs2,
-    c2 <> Sskip ->
-    ((exists mcs1, step (c1, s1) mcs1 /\ mcs2 = fmap_Sseq mcs1 c2) <->
-     step (Ssequence c1 c2, s1) mcs2);
-  step_Ssequence2: forall c s mcs,
-    step (c, s) mcs <->
-    step (Ssequence Sskip c, s) mcs;
-  step_progress: forall c s, c = Sskip <-> exists mcs, step (c, s) mcs
-}.
-
-Inductive iter_step {Imp: ImperativeProgrammingLanguage} {nImp: NormalImperativeProgrammingLanguage Imp} {MD: Model} {sss: SmallStepSemantics Imp MD}: cmd * state + exceptional_state -> cmd * state + exceptional_state -> Prop :=
-| iter_step_refl: forall mcs, iter_step mcs mcs
-| iter_step_step: forall cs mcs1 mcs2, step cs mcs1 -> iter_step mcs1 mcs2 -> iter_step (inl cs) mcs2.
-
-Definition access {Imp: ImperativeProgrammingLanguage} {nImp: NormalImperativeProgrammingLanguage Imp} {MD: Model} {sss: SmallStepSemantics Imp MD} (ms_init: state + exceptional_state) (c: cmd) (ms_end: state + exceptional_state): Prop :=
-  iter_step (fmap_pair_cmd ms_init c) (fmap_pair_cmd ms_end Sskip).
-*)
