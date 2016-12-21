e57d2fabbd123207f52381845b48040f6b115e02
QinxiangCao <caoqinxiang@gmail.com>
2016-12-21 02:52:56 -0500

    Add some generators on operation semantics.

--- HoareLogic/SequentialSemantics.v
+++ HoareLogic/SequentialSemantics.v
@@ -1,3 +1,4 @@
+Require Import Coq.Relations.Relation_Operators.
 Require Import Logic.MinimunLogic.LogicBase.
 Require Import Logic.PropositionalLogic.KripkeSemantics.
 Require Import Logic.SeparationLogic.SeparationAlgebra.
@@ -11,6 +12,15 @@ Inductive MetaState (state: Type): Type :=
 Arguments Error {_}.
 Arguments NonTerminating {_}.
 Arguments Terminating {_} _.
+
+Inductive lift_relation {state: Type} (R: state -> MetaState state -> Prop):
+  MetaState state-> MetaState state -> Prop :=
+| lift_relation_Error:
+    lift_relation R Error Error
+| lift_relation_NonTerminating:
+    lift_relation R NonTerminating NonTerminating
+| lift_relation_Terminating:
+    forall s ms, R s ms -> lift_relation R (Terminating s) ms.
 
 Inductive lift_Korder
           {state: Type}
@@ -50,17 +60,30 @@ Class BigStepSemantics (P: ProgrammingLanguage) (state: Type): Type := {
   access: state -> cmd -> MetaState state -> Prop
 }.
 
-Inductive lift_access
+Class SmallStepSemantics (P: ProgrammingLanguage) (state: Type): Type := {
+  step: cmd * state -> MetaState (cmd * state) -> Prop
+}.
+
+Class SimpleSmallStepSemantics (P: ProgrammingLanguage) (state: Type): Type := {
+  simple_step: cmd * state -> cmd * state -> Prop
+}.
+
+Definition lift_access
           {P: ProgrammingLanguage}
           {state: Type}
-          {BSS: BigStepSemantics P state}:
-  MetaState state -> cmd -> MetaState state -> Prop :=
-| lift_access_Error:
-    forall c, lift_access Error c Error
-| lift_access_NonTerminating:
-    forall c, lift_access NonTerminating c NonTerminating
-| lift_access_Terminating:
-    forall s c ms, access s c ms -> lift_access (Terminating s) c ms.
+          {BSS: BigStepSemantics P state}
+          (ms1: MetaState state)
+          (c: cmd)
+          (ms2: MetaState state): Prop :=
+  lift_relation (fun s => access s c) ms1 ms2.
+
+Definition lift_step
+          {P: ProgrammingLanguage}
+          {state: Type}
+          {SSS: SmallStepSemantics P state}
+          (mcs1: MetaState (cmd * state))
+          (mcs2: MetaState (cmd * state)): Prop :=
+  lift_relation (fun cs => step cs) mcs1 mcs2.
 
 Definition safe
            {P: ProgrammingLanguage}
@@ -84,3 +107,29 @@ Class NormalBigStepSemantics (P: ProgrammingLanguage) (state: Type) (BSS: BigSte
   access_defined: forall s c, exists ms, access s c ms
 }.
 
+Class NormalSmallStepSemantics (P: ProgrammingLanguage) (state: Type) (BSS: SmallStepSemantics P state): Type := {
+  step_defined: forall cs, exists mcs, step cs mcs
+}.
+
+(****************************************************)
+(* Generators                                       *)
+(****************************************************)
+
+Instance SSS_SimpleSSS {P: ProgrammingLanguage} {state: Type} (SSSS: SimpleSmallStepSemantics P state): SmallStepSemantics P state :=
+  Build_SmallStepSemantics _ _
+   (fun cs mcs =>
+      match mcs with
+      | Terminating cs0 => simple_step cs cs0
+      | NonTerminating => False
+      | Error => forall cs0, ~ simple_step cs cs0
+      end).
+
+Instance BSS_SSS {P: ProgrammingLanguage} {Imp: ImperativeProgrammingLanguage P} {state: Type} (SSS: SmallStepSemantics P state): BigStepSemantics P state :=
+  Build_BigStepSemantics _ _
+   (fun s c ms =>
+      clos_refl_trans _ lift_step (Terminating (c, s))
+        match ms with
+        | Terminating s0 => Terminating (Sskip, s0)
+        | NonTerminating => NonTerminating
+        | Error => Error
+        end).
